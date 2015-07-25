bbabca8c2452d702530bf59a49147c8f0e2c9c1e
Myeonggu Sa <quest4i@me.com>
2015-07-26 02:08:58 +0900

    템플릿을 이용하도록 홈페이지 뷰를 리팩터링

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpRequest
-from django.http import HttpResponse
 
 
 def home_page(request):
 
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
